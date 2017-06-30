5504e9a7322272a9006acd0acc7f3f769ee82440
MacKirby <johnny187896@live.co.uk>
2017-06-30 05:57:43 +0100

    Finally fixed the SQL stuff.  CoNDOR races button re-enabled.

--- MacSG/frmSchedule.vb
+++ MacSG/frmSchedule.vb
@@ -3,15 +3,6 @@ Imports System.IO
 
 Public Class frmSchedule
 
-    Dim epoch As New DateTime(1970, 1, 1)
-    Dim week1Start As New DateTime(2017, 2, 25, 23, 0, 0)
-    Dim week2Start As New DateTime(2017, 3, 4, 23, 0, 0)
-    Dim week3Start As New DateTime(2017, 3, 11, 23, 0, 0)
-    Dim week4Start As New DateTime(2017, 3, 18, 23, 0, 0)
-    Dim week5Start As New DateTime(2017, 3, 25, 23, 0, 0)
-    Dim weekCurrent As DateTime = DateTime.UtcNow
-    Dim weekNumber As Integer
-
     Private Sub frmSchedule_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Dim frmMainLocation As Point = frmMain.Location
@@ -29,23 +20,22 @@ Public Class frmSchedule
         Dim dt As New DataTable
         Dim bSource As New BindingSource
 
-        Dim sqlconn = New MySqlConnection("server=condor.host;userid=necrobot-read;password=necrobot-read;database=conduit_19;")
+        Dim sqlconn = New MySqlConnection("server=condor.host;userid=necrobot-read;password=necrobot-read;database=necrobot;")
         Dim filterText As String = txtFilter.Text
 
         Try
             sqlconn.Open()
             Dim query As String
-            query = "call schedule(""" + filterText + """);"
+            query = "call current_event_schedule(""" + filterText + """);"
 
             Dim sqlcommand = New MySqlCommand(query, sqlconn)
             sqlcommand.Parameters.Add("@filter", MySqlDbType.VarChar).Value = filterText
             sqlDataAdapter.SelectCommand = sqlcommand
             sqlDataAdapter.Fill(dt)
             bSource.DataSource = dt
-            dgvSchedule.Columns(0).DataPropertyName = "timestamp"
-            dgvSchedule.Columns(1).DataPropertyName = "racer_1_rtmp_name"
-            dgvSchedule.Columns(2).DataPropertyName = "racer_2_rtmp_name"
-            dgvSchedule.Columns(3).DataPropertyName = "league"
+            dgvSchedule.Columns(0).DataPropertyName = "Time"
+            dgvSchedule.Columns(1).DataPropertyName = "Racer 1"
+            dgvSchedule.Columns(2).DataPropertyName = "Racer 2"
 
             dgvSchedule.AutoGenerateColumns = False
             sqlDataAdapter.Update(dt)
@@ -83,15 +73,4 @@ Public Class frmSchedule
 
     End Sub
 
-    Private Sub colourRows() Handles dgvSchedule.Sorted
-        For Each row As DataGridViewRow In dgvSchedule.Rows
-            If row.Cells("league").Value.Equals("Blood") Then
-                row.DefaultCellStyle.BackColor = Color.FromArgb(234, 153, 153)
-            End If
-        Next
-    End Sub
-
-    Private Sub colourRows(sender As Object, e As EventArgs) Handles dgvSchedule.Sorted
-
-    End Sub
 End Class
